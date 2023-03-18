2b97fcd57ae743f9a1c71f70d2bae15696f3f05b
jrginn <jrginn47@gmail.com>
2023-03-18 17:14:54 -0400

    added checkMesh

--- PA1/topologies/Topologies.java
+++ PA1/topologies/Topologies.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class Topologies {
 
     public static int[][] readAdjmat() {
+        // creates scanner to read input file
         File f = new File("./PA1/topologies/input.txt");
         Scanner scan;
         try {
@@ -15,26 +16,18 @@ public class Topologies {
             System.out.println("input.txt could not be found");
             return null;
         }
-        // // gets first line
-        // String first = scan.nextLine();
-        // // finds number of 0/1s in first line and determines n
-        // String[] firstBits = first.split(" ");
-        // int n = firstBits.length;
-        // int[][] adjmat = new int[n][n];
-        // for(int i = 0; i < n; i++) {
-        //     adjmat[1][i] = Integer.parseInt(firstBits[i]);
-        // }
-        // populates rest of adjmat
         ArrayList<String[]> lines = new ArrayList<>();
         while(scan.hasNextLine()) {
             // adds array of 0s and 1s to lines
             lines.add(scan.nextLine().split(" "));
         }
+        // ensures file has at least one entry
         if(lines.size() < 1) {
             System.out.println("Invalid file");
             scan.close();
             return null;
         }
+        // converts ArrayList of string arrays to int 2d array
         int n = lines.get(0).length;
         int[][] adjmat = new int[n][n];
         for(int i = 0; i < n; i++) {
@@ -60,8 +53,20 @@ public class Topologies {
         }
     }
 
+    public static boolean checkMesh(int[][] mat) {
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[0].length; j++) {
+                if((mat[i][j] == 0 && i != j) || (mat[i][j] == 1 && i == j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
-        printMatrix(readAdjmat());
+        //printMatrix(readAdjmat());
+        System.out.println(checkMesh(readAdjmat()));
     }
 }
