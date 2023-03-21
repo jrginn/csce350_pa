7a6437e94f39bd7a60ffdd401ac1a3da19c4a8cf
jrginn <jrginn47@gmail.com>
2023-03-21 15:50:25 -0400

    added readme, fixed QS package issues

--- PA1/bonus/Bonus.java
+++ PA1/bonus/Bonus.java
@@ -1,12 +1,102 @@
 package PA1.bonus;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-
-import PA1.quicksort.*;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Bonus {
+
+
+    /**
+     * Implements quicksort according to the texbook
+     * @param arr array of doubles to be sorted
+     * @param l left subarray bound
+     * @param r right subarray bound
+     */
+    public static void quickSort(double[] arr, int l, int r) {
+        if(l < r) {
+            int s = partition(arr, l, r);
+            quickSort(arr, l, s-1);
+            quickSort(arr, s+1, r);
+        }
+    }
+
+    /**
+     * Partitions subarray arr[l, ..., r] with pivot arr[l]
+     * @param arr array to be sorted
+     * @param l left bound on subarray
+     * @param r right bound on subarray
+     * @return split index of partition's correct place
+     */
+    public static int partition(double[] arr, int l, int r) {
+        double p = arr[l];
+        int i = l;
+        int j = r;
+        while(i < j) {
+            while(arr[i] <= p && i < r) {
+                i++;
+            }
+            
+            while(arr[j] > p && i >= l) {
+                j--;
+            }
+            swap(arr, i, j);
+        }
+        swap(arr, i, j);
+        swap(arr, l, j);
+        return j;
+    }
+
+    /**
+     * Swaps arr[a] with arr[b]
+     * @param arr array of doubles
+     * @param a index to swap
+     * @param b other index to swap
+     */
+    public static void swap(double[] arr, int a, int b) {
+        double temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    /**
+     * Creates a double array based on the contents of input.txt
+     * @return array with doubles in input.txt
+     */
+    public static double[] readArray(String filepath) {
+        File f = new File(filepath);
+        Scanner scan;
+        try {
+            scan = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("input.txt could not be found");
+            return null;
+        }
+        String[] words = scan.nextLine().split("\\s");
+        double[] arr = new double[words.length];
+        for(int i = 0; i < words.length; i++) {
+            arr[i] = Double.parseDouble(words[i]);
+        }
+        scan.close();
+        return arr;
+    }
+
+    /**
+     * Adds each element of arr to a line
+     * @param arr array of doubles
+     * @return space-separated string containing arr elements
+     */
+    public static String writeArray(double[] arr) {
+        String str = "";
+        DecimalFormat numberFormat = new DecimalFormat("#.###");
+        for(Double d: arr) {
+            str += numberFormat.format(d) + " ";
+        }
+        return str;
+    }
 
     /**
      * Creates an array of random doubles and saves it to a file
@@ -19,7 +109,7 @@ public class Bonus {
         for(int i = 0; i < size; i++) {
             arr[i] = Math.random() * 10;
         }
-        String str = QS.writeArray(arr);
+        String str = writeArray(arr);
         try {
             f.createNewFile();
             FileWriter writer = new FileWriter(f);
@@ -51,9 +141,9 @@ public class Bonus {
             int size = (int)Math.pow(10, i);
             long total = 0;
             for(int j = 0; j < 100; j++) {
-                double[] arr = QS.readArray("./PA1/bonus/" + size + "/" + j + ".txt");
+                double[] arr = readArray("./PA1/bonus/" + size + "/" + j + ".txt");
                 long start = System.nanoTime();
-                QS.quickSort(arr, 0, arr.length-1);
+                quickSort(arr, 0, arr.length-1);
                 total += (System.nanoTime() - start);
             }
             avgs[i-1] = total/100;
---
+++ PA1/bonus/README.md
@@ -0,0 +1,10 @@
+Before running the analysis:
+Generate random input files if they are not already present
+    Running generateAllFiles() is sufficient
+Ensure all input files exist
+
+To run:
+Clone the github repo into the current directory and change directory into csce350_pa
+From the current directory run
+    javac -target 1.7 -source 1.7 ./PA1/bonus/Bonus.java
+    java PA1.bonus.Bonus
