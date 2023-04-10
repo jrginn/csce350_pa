d16b596a646daaeb7d15534f3802d9b18f4ee350
jrginn <jrginn47@gmail.com>
2023-04-10 16:07:54 -0400

    finished testing basic heap functionality

--- PA2/heap/HeapTest.java
+++ PA2/heap/HeapTest.java
@@ -7,30 +7,37 @@ public class HeapTest {
         // MaxHeap mxheap = new MaxHeap(arr);
         // System.out.println(mxheap);
         // System.out.println(checkMaxHeap(mxheap));
-        int[] arr2 = {10, 7, 15, 17, 12, 20, 6, 32};
-        MinHeap mnheap = new MinHeap(arr2);
-        System.out.println(mnheap);
-        // int[] arr = new int[10];
-        // int failCount = 0;
-        // for(int i = 0; i < 999; i++) {
-        //     for(int j = 0; j < arr.length; j++) {
-        //         arr[j] = (int)(Math.random() * 100);
-        //     }
-        //     MaxHeap heap = new  MaxHeap(arr);
-        //     if(!checkMaxHeap(heap)) {
-        //         failCount++;
-        //     }
-        //     else if((i - failCount) % 20 == 0) {
-        //         System.out.println(heap);
-        //     }
-        // }
-        // System.out.println(failCount);
+        // int[] arr2 = {10, 7, 15, 17, 12, 20, 6, 32};
+        // MinHeap mnheap = new MinHeap(arr2);
+        // System.out.println(mnheap);
+        int[] arr = new int[10];
+        int failCount = 0;
+        for(int i = 0; i < 999; i++) {
+            for(int j = 0; j < arr.length; j++) {
+                arr[j] = (int)(Math.random() * 100);
+            }
+            MinHeap heap = new  MinHeap(arr);
+            if(!checkMinHeap(heap)) {
+                failCount++;
+            }
+        }
+        System.out.println(failCount);
     }
 
     public static boolean checkMaxHeap(MaxHeap mh) {
         int[] heap = mh.getHeap();
         for(int i = 1; 2 * i + 1 < heap.length; i++) {
             if(heap[i] < heap[2 * i] || heap[i] < heap[2 * i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkMinHeap(MinHeap mh) {
+        int[] heap = mh.getHeap();
+        for(int i = 1; 2 * i + 1 < heap.length; i++) {
+            if(heap[i] > heap[2 * i] || heap[i] > heap[2 * i + 1]) {
                 return false;
             }
         }
