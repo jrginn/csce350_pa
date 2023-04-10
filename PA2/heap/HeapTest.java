e27fa682932d6edd469c93f227d4edef3020a97c
jrginn <jrginn47@gmail.com>
2023-04-10 10:39:11 -0400

    intermediate work on heap

--- PA2/heap/HeapTest.java
+++ PA2/heap/HeapTest.java
@@ -3,9 +3,11 @@ package PA2.heap;
 public class HeapTest {
     
     public static void main(String[] args) {
-        int[] arr = {1, 8, 6, 5, 3, 7, 4};
-        MaxHeap heap = new MaxHeap(arr);
-        System.out.println(heap);
+        int[] arr = {35, 33, 42, 10, 14, 19, 27, 44, 26, 31};
+        MaxHeap mxheap = new MaxHeap(arr);
+        System.out.println(mxheap);
+        MinHeap mnheap = new MinHeap(arr);
+        System.out.println(mnheap);
     }
 
 }
--- PA2/heap/MinHeap.java
+++ PA2/heap/MinHeap.java
@@ -1,15 +1,15 @@
 package PA2.heap;
 
-public class MaxHeap {
+public class MinHeap {
     
     private int[] heap;
 
-    public MaxHeap(int[] heap) {
+    public MinHeap(int[] heap) {
         this.heap = heapify(heap);
     }
 
     public static int[] heapify(int[] heap) {
-        // implements HeapBottomUp from textbook
+        // implements HeapBottomUp from textbook for MinHeap
         // Subtracts one from indices to account for book's one-indexing
         for(int i = heap.length/2; i >= 1; i--) {
             int k = i;
@@ -18,18 +18,18 @@ public class MaxHeap {
             // checks parental dominance until fully satisfied
             while(!isHeap && 2 * k <= heap.length) {
                 int j = 2 * k;
-                // two children, finds largest
+                // two children, finds smallest
                 if(j < heap.length) {
-                    if(heap[j - 1] < heap[j]) {
+                    if(heap[j - 1] > heap[j]) {
                         j = j++;
                     }
                 }
-                // parental dominance is satisfied
-                if(v  >= heap[j - 1]) {
+                // parental dominance is violated (good for MinHeap)
+                if(v  <= heap[j - 1]) {
                     isHeap = true;
                 }
                 else {
-                    // swaps with larger child
+                    // swaps with smaller child
                     heap[k - 1] = heap[j - 1];
                     k = j;
                 }
