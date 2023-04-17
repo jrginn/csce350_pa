538ff6141e191c88d57da689cc1b4dc31f8f532f
jrginn <jrginn47@gmail.com>
2023-04-17 10:54:23 -0400

    created Floyd's algorithm

---
+++ PA4/floyd/Floyd.java
@@ -0,0 +1,81 @@
+package PA4.floyd;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Floyd {
+    
+    /**
+     * Takes input from input.txt to create adjacency matrix
+     * @return 2d double adjacency matrix
+     */
+    public static double[][] readAdjmat() {
+        // creates scanner to read input file
+        File f = new File("./PA4/floyd/input.txt");
+        Scanner scan;
+        try {
+            scan = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("input.txt could not be found");
+            return null;
+        }
+        ArrayList<String[]> lines = new ArrayList<>();
+        while(scan.hasNextLine()) {
+            // adds array of 0s and 1s to lines
+            lines.add(scan.nextLine().split("\\s"));
+        }
+        // ensures file has at least one entry
+        if(lines.size() < 1) {
+            System.out.println("Invalid file");
+            scan.close();
+            return null;
+        }
+        // converts ArrayList of string arrays to int 2d array
+        int n = lines.get(0).length;
+        double[][] adjmat = new double[n][n];
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                adjmat[i][j] = Double.parseDouble(lines.get(i)[j]);
+            }
+        }
+        scan.close();
+        return adjmat;
+    }
+
+    /**
+     * Prints an adjacency matrix row by row
+     * @param mat adjacency matrix
+     */
+    public static void printMatrix(double[][] mat) {
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[i].length; j++) {
+                System.out.print(mat[i][j]);
+                if(j != mat[i].length - 1) {
+                    System.out.print(" ");
+                }
+            }
+            if(i != mat.length - 1) {
+                System.out.print("\n");
+            }
+        }
+    }
+
+    public static double[][] floydPaths(double[][] mat) {
+        double[][] lengths = mat.clone();
+        int n = lengths.length;
+        for(int k = 0; k < n; k++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    lengths[i][j] = Math.min(lengths[i][j], lengths[i][k] + lengths[k][j]);
+                }
+            }
+        }
+        return lengths;
+    }
+
+    public static void main(String[] args) {
+        printMatrix(floydPaths(readAdjmat()));
+    }
+}
