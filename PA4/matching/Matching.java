e916ee06c4279bce4a71f73a6be07007ac4ba705
jrginn <jrginn47@gmail.com>
2023-04-18 21:55:07 -0400

    creates non-maximal matching

--- PA4/matching/Matching.java
+++ PA4/matching/Matching.java
@@ -1,12 +1,93 @@
 package PA4.matching;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class Matching {
+
+    public static HashSet<Integer> getV() {
+        HashSet<Integer> v = new HashSet<Integer>();
+        File f = new File("./PA4/matching/input.txt");
+        Scanner scan;
+        try {
+            scan = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("input.txt could not be found");
+            return null;
+        }
+        String[] nums = scan.nextLine().split("\\s");
+        //int[] vals = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            v.add(Integer.parseInt(nums[i]));
+        }
+        scan.close();
+        return v;
+    }
+
+    public static HashSet<Integer> getU() {
+        HashSet<Integer> u = new HashSet<Integer>();
+        File f = new File("./PA4/matching/input.txt");
+        Scanner scan;
+        try {
+            scan = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("input.txt could not be found");
+            return null;
+        }
+        //skips first line (V)
+        scan.nextLine();
+        String[] nums = scan.nextLine().split("\\s");
+        //int[] vals = new int[nums.length];
+        for(int i = 0; i < nums.length; i++) {
+            u.add(Integer.parseInt(nums[i]));
+        }
+        scan.close();
+        return u;
+    }
+
+    /**
+     * Takes input from input.txt to create adjacency matrix
+     * @return 2d double adjacency matrix
+     */
+    public static int[][] readAdjmat() {
+        // creates scanner to read input file
+        File f = new File("./PA4/matching/input.txt");
+        Scanner scan;
+        try {
+            scan = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            System.out.println("input.txt could not be found");
+            return null;
+        }
+        ArrayList<String[]> lines = new ArrayList<>();
+        while(scan.hasNextLine()) {
+            // adds array of 0s and 1s to lines
+            lines.add(scan.nextLine().split("\\s"));
+        }
+        // ensures file has at least one entry
+        if(lines.size() < 1) {
+            System.out.println("Invalid file");
+            scan.close();
+            return null;
+        }
+        // converts ArrayList of string arrays to int 2d array
+        int n = lines.get(2).length;
+        int[][] adjmat = new int[n][n];
+        // skips V and U lines
+        for(int i = 2; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                adjmat[i-2][j] = Integer.parseInt(lines.get(i)[j]);
+            }
+        }
+        scan.close();
+        return adjmat;
+    }
 
     public static ArrayList<Integer> getAdjVerts(int[][]mat, int vertex) {
         ArrayList<Integer> verts = new ArrayList<Integer>();
@@ -45,16 +126,47 @@ public class Matching {
                 ArrayList<Integer> adjs = getAdjVerts(mat, w);
                 for(Integer u: adjs) {
                     // u is free
-                    if(!m.containsKey(u)) {
+                    if(!m.containsValue(u)) {
                         // augment
                         m.put(w, u);
                         int v = w;
+                        while(labels[v] != -1) {
+                            u = labels[v];
+                            m.remove(v);
+                            v = labels[u];
+                            m.put(v, u);
+                        }
+                        revertLabels(labels);
+                        // reinitializes queue with free verts
+                        q.clear();
+                        for(Integer f: a) {
+                            if(!m.containsKey(f)) {
+                                q.add(f);
+                            }
+                        }
+                        break;
+                    }
+                    else {
+                        if((m.containsKey(w) && m.get(w) != u) && labels[u] == -1) {
+                            labels[u] = w;
+                            q.add(u);
+                        }
                     }
                 }
+            }
+            else {
+                labels[m.get(w)] = w;
+                q.add(m.get(w));
             }
         }
         return m;
     }
 
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> match = maxMatch(getV(), getU(), readAdjmat());
+        for(Integer k: match.keySet()) {
+            System.out.println(k + " " + match.get(k));
+        }
+    }
 
 }
